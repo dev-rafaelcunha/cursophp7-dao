006b1f9b10f01bf620991f96be10e0e79add6db7
Rafa Souza <rafaelsouza-nvg@hotmail.com>
2022-08-08 19:52:27 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 
 require_once('config.php');
 
-$root = new Usuario();
-$root->loadById(3);
+// CHAMA UM USUARIO POR ID SELECIONADO //
+// $root = new Usuario();     
+// $root->loadById(3);
+// echo $root;
 
-echo $root;
+// CHAMA TODOS OS ID //
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
+// CHAMA A LISTA DE USUÁRIO PELO LOGIN //
+// $search = Usuario::search('jo');
+// echo json_encode($search);
+
+// CHAMA UM USUÁRIO USANDO O LOGIN E SENHA "VALIDANDO" //
+$usuario = new Usuario();
+$usuario->login('rafa', '123');
+echo $usuario;
